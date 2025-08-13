f7a8e90cc033a9c0a8b6d51dd5718792143a126c
Hdkskd <158311431+ziayzu@users.noreply.github.com>
2025-08-13 12:57:33 +0530

    Create DownloadManager.kt

---
+++ android/app/src/main/java/com/ziayzu/launcher/DownloadManager.kt
@@ -0,0 +1,8 @@
+package com.ziayzu.launcher
+
+class DownloadManager {
+    fun downloadGameFiles() {
+        // Placeholder for download logic
+        println("Downloading game files...")
+    }
+}
