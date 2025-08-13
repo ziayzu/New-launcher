6f165bda53a347cf7ca5e6016bd5d60ae1f15249
Hdkskd <158311431+ziayzu@users.noreply.github.com>
2025-08-13 12:57:01 +0530

    Create AuthManager.kt

---
+++ android/app/src/main/java/com/ziayzu/launcher/AuthManager.kt
@@ -0,0 +1,9 @@
+package com.ziayzu.launcher
+
+class AuthManager {
+    fun login(): Boolean {
+        // Placeholder for Microsoft login
+        println("Logging in...")
+        return true
+    }
+}
